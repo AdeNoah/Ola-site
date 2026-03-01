cc195b7231b2daf099952dd3f72bb62a71ceaea1
Noah Adegboye <87646870+AdeNoah@users.noreply.github.com>
2026-03-01 20:32:40 +0100

    rearranged the filing structure to ensure the images are imported and exported correctly and hence displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+import { dommyImages } from "./images";
 
+interface ImageItem {
+  id: number;
+  name: string;
+  url: string;
+}
 
 function App() {
 
+  const [images, setImages] = useState<ImageItem[]>([]); 
+
+  useEffect(() => {
+    setImages(dommyImages.images)
+  }, [])
+
   return (
     <>
-      
+    <div className="flex justify-center">
+      {images.length > 0 ?(
+        <ul>
+          {images.map((img) => (
+            <li key={img.id}>
+              <img src={img.url} alt={img.name} width="800" height="600" />
+            </li>
+          ))}
+        </ul>
+      ) : <h1>No images found</h1>}
+    </div>
+
     </>
   )
 }
---
+++ src/images.ts
@@ -0,0 +1,101 @@
+import photo1 from "/images/01.jpeg"
+import photo2 from "/images/02.jpeg"
+import photo3 from "/images/03.jpeg"
+import photo4 from "/images/04.jpeg"
+import photo5 from "/images/05.jpeg"
+import photo6 from "/images/06.jpeg"
+import photo7 from "/images/07.jpeg"
+import photo8 from "/images/08.jpeg"
+import photo9 from "/images/09.jpeg"
+import photo10 from "/images/10.jpeg"
+import photo11 from "/images/11.jpeg"
+import photo12 from "/images/12.jpeg"
+import photo13 from "/images/13.jpeg"
+import photo14 from "/images/14.jpeg"
+import photo15 from "/images/15.jpeg"
+import photo16 from "/images/16.jpeg"
+
+export const dommyImages = {
+    "images": [
+        {
+            "id": 0,
+            "name": "one",
+            "url": photo1
+        },
+        {
+            "id": 1,
+            "name": "two",
+            "url": photo2
+        },
+        {
+            "id": 2,
+            "name": "three",
+            "url": photo3
+        },
+        {
+            "id": 3,
+            "name": "four",
+            "url": photo4
+        },
+        {
+            "id": 4,
+            "name": "five",
+            "url": photo5
+        },
+        {
+            "id": 5,
+            "name": "six",
+            "url": photo6 
+        },
+        {
+            "id": 6,
+            "name": "seven",
+            "url": photo7
+        },
+        {
+            "id": 7,
+            "name": "eight",
+            "url": photo8
+        },
+        {
+            "id": 8,
+            "name": "nine",
+            "url": photo9
+        },
+        {
+            "id": 9,
+            "name": "ten",
+            "url": photo10
+        },
+        {
+            "id": 10,
+            "name": "eleven",
+            "url": photo11
+        },
+        {
+            "id": 11,
+            "name": "twelve",
+            "url": photo12
+        },
+        {
+            "id": 12,
+            "name": "thirteen",
+            "url": photo13
+        },
+        {
+            "id": 13,
+            "name": "fourteen",
+            "url": photo14
+        },
+        {
+            "id": 14,
+            "name": "fifteen",
+            "url": photo15
+        },
+        {
+            "id": 15,
+            "name": "sixteen",
+            "url": photo16
+        }
+    ]
+}
